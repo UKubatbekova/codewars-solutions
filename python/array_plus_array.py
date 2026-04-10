2e8efbe76e5dfa86a5697a1091d44531a11edf42
Umsu Kubatbekova <umsunai.kubatbekova@gmail.com>
2026-04-09 21:49:20 -0700

    Add Day 6 Codewars solutions

---
+++ python/array_plus_array.py
@@ -0,0 +1,6 @@
+# https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/train/python
+
+def array_plus_array(arr1,arr2):
+    first_array = sum(arr1)
+    second_array = sum(arr2)
+    return first_array + second_array
