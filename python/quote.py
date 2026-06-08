68b14da70ded00f994edfec7925023055973a152
Umsu Kubatbekova <umsunai.kubatbekova@gmail.com>
2026-06-07 19:24:11 -0700

    Add Day 65 Codewars solutions

---
+++ python/quote.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/582dafb611d576b745000b74/train/python
+
+def quote(fighter):
+    if fighter.casefold() == "George Saint Pierre".casefold():
+        return "I am not impressed by your performance."
+    elif fighter.casefold() == "Conor McGregor".casefold():
+        return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
